c0a2256511f90e878ff12f2737d72f765bf78cd8
Colin Caufield <ccaufield@flipboard.com>
2019-06-24 22:21:49 -0700

    Initial commit.

---
+++ AdManagerCustomRenderingExample/AppDelegate.m
@@ -0,0 +1,12 @@
+//  Copyright (c) 2015 Google. All rights reserved.
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    return YES;
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '8.0'
+
+target 'AdManagerCustomRenderingExample' do
+  pod 'Google-Mobile-Ads-SDK', '7.44.0'
+end
